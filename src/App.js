b1615c34254363aba92a00ec82c4b7f59f020565
VamsiSatyaSai <vamsivanama61@gmail.com>
2024-09-30 14:07:08 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="bg-container">
+        <form action="https://smoketreebackend-aryn.onrender.com/register" method="POST" className="user-form">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+
+          <label htmlFor="address">Address:</label>
+          <input type="text" id="address" name="address" required />
+
+          <label htmlFor="city">City:</label>
+          <input type="text" id="city" name="city" required />
+
+          <label htmlFor="state">State:</label>
+          <input type="text" id="state" name="state" required />
+
+          <label htmlFor="zipcode">Zipcode:</label>
+          <input type="text" id="zipcode" name="zipcode" required />
+
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
